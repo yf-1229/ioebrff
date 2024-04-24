bb4b4bc747d196c128a9a7b91f614186b9395f8c
KituneG0n <yf1229@proton.me>
2024-04-25 07:29:01 +0900

    Add menu

--- main.bash
+++ main.bash
@@ -6,32 +6,29 @@ main_menu() {
   choice=$(whiptail --title "ioebrff" --menu "How can I help today?" 15 60 4 \
     "1" "Edit File" \
     "2" "Save File" \
-    "3" "Run Code" \
-    "4" "Terminal" \
-    "5" "Document" \
+    "3" "Document" \
+    "4" "Run Code" \
+    "5" "EXIT" \
     3>&1 1>&2 2>&3)
 
   case $choice in
     1) edit_file ;;
     2) save_file ;;
-    3) run_code ;; # Ruby Only
-    4) Terminal ;;
-    5) Document ;;
+    3) document ;;
+    4) run_code ;;
+    5) exit ;;
     *) echo "Invailid option" ;;
   esac 
 }
 
 edit_file() {
   filename=$(whiptail --title "Edit File" --inputbox "Enter File Path:" 10 60 3>&1 1>&2 2>&3)
-  
-  # ファイルが存在するか確認
+
   if [ -f "$filename" ]; then
     export filename
     screen ruby main.rb
   else
-    filename=$(whiptail --title "Edit File" --inputbox "Enter File Path:" 10 60 3>&1 1>&2 2>&3)
-    export filename
-    screen ruby main.rb
+    whiptail --title "Error" --msgbox "File not found: $filename" 10 60
   fi
   
   main_menu
