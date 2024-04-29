5b638834930905df61172000b8b6c5bb0bd6c698
KituneG0n <yf1229@proton.me>
2024-04-29 21:15:10 +0900

    Delete some

--- main.rb
+++ main.rb
@@ -1,20 +1,6 @@
 require "curses"
-require "./editwindow"
 
 filename = ENV['FILENAME_IOEBRFF']
 
-Curses.init_screen
-Curses.cbreak
-Curses.noecho
 
-# Base window
-base_window = Curses.stdscr
-# Edit Window
-edit_window = EditWindow.new(base_window)
-
-# Open file
-edit_window.display(filename)
-edit_window.getch
-
-Curses.close_screen
 
